22f812136dbc0e416c33e6d7ab5a2248e05af042
hexmarsh <antabi39@gmail.com>
2026-08-31 20:59:08 -0400

    Update Dashboard

--- frontend/src/pages/DashBoard.tsx
+++ frontend/src/pages/DashBoard.tsx
@@ -1,16 +1,36 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
+import { Card } from "@/components/ui/card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChartPie,
+  faGear,
+  faTicket,
+  faUsersGear,
+} from "@fortawesome/free-solid-svg-icons";
+import type { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+type DashBoardItemProps = {
+  icon: IconProp;
+  name: string;
+};
+
+function DashBoardItem({ icon, name }: DashBoardItemProps) {
+  return (
+    <Card className="size-50 flex flex-col items-center justify-center hover:-translate-y-2 hover:text-primary hover:shadow-2xl active:translate-y-0 active:shadow-md cursor-pointer">
+      <FontAwesomeIcon icon={icon} className="text-5xl" />
+      <span className="text-lg font-medium text-center">{name}</span>
+    </Card>
+  );
+}
 
 function DashBoard() {
-
-    return (
-        <div className="flex">
-            <Card className="">
-                <Label></Label>
-            </Card>
-        </div>
-    );
+  return (
+    <div className="flex w-screen h-screen items-center justify-center gap-6">
+      <DashBoardItem icon={faTicket} name="Manage Tickets" />
+      <DashBoardItem icon={faUsersGear} name="Manage Users" />
+      <DashBoardItem icon={faChartPie} name="Reports" />
+      <DashBoardItem icon={faGear} name="Settings" />
+    </div>
+  );
 }
 
 export default DashBoard;
